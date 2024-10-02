75c9283285807c9568ed664c0549791f3ae44e4f
Alee <alexandresilvanascimento177@gmail.com>
2024-10-02 15:24:49 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function saudacao() {
+    alert("♣ Bem-vindo ao site dos Indigenas!! ♥");
+}
+
+function solicitarNome() {
+    var nome = prompt("Qual é o nome do indigena?");
+    
+    if (nome != null && nome !== "") {
+        alert("Olá, " + nome + "!");
+    } else {
+        alert("Você não inseriu seu nome de indigena.");
+    }
+}
+function JavaScript() {
+    alert("♣ O que é Javascript? Javascript é uma linguagem de programação leve que os desenvolvedores usam para criar interações mais dinâmicas ao desenvolver páginas da Web, aplicativos, servidores, jogos, etc. Ele cria elementos para melhorar a interação dos visitantes do site com as páginas da web, como menus suspensos, gráficos animados e cores de fundo dinâmicas.Geralmente o Javascript é usado como complemento às linguagens com HTML, CSS e PHP,  mas a  linguagem funciona bem também com CSS na formatação de elementos HTML. No entanto, ainda mantém a interação do usuário, algo que o CSS não pode fazer sozinho.Como a linguagem surgiu e tornou-se popular na programação? Javascript surgiu em 1995 e foi criada por Brendan Eich, a pedido da empresa Netscape (um dos primeiros navegadores) com o objetivo de validar formulários HTML. Como os browsers ainda eram estéticos, essa linguagem foi revolucionária para a época.Em 1996, a Microsoft criou uma linguagem idêntica para ser usada no Internet Explorer. A Netscape então regularizou a linguagem através da organização internacional ECMA, companhia especializada em padrões e regras. Com o tempo, a linguagem foi sendo modificada e hoje em dia é “Client-side”, ou seja o que ocorre no dispositivo do usuário final, o cliente. Isso inclui o que o usuário vê, como texto, imagens e o restante da interface, além de todas as ações que um aplicativo executa no navegador do usuário. ♥");
+}
+function Html(){
+    alert("♣ HTML com JAVA SCRIPT. Se você quer exibir no seu site um conteúdo estático como, por exemplo, um conjunto de imagens, então o HTML pode servir muito bem para você. Entretanto, páginas estáticas estão cada vez mais se tornando coisa do passado. A maior parte dos conteúdos de hoje é interativo e possui slideshows chamativos, formulários e menus. Eles melhoram a experiência do usuário e adicionam dinamicidade para um site, independente do host que você usar.E isso tudo é conseguido através do uso de linguagens de programação, sendo a JavaScript uma das mais famosas nesse sentido. Ela permite que os desenvolvedores criem sites que interajam com os usuários e vice-versa.Embora existam muitas outras linguagens disponíveis, nenhuma delas é tão popular quanto a JavaScript. E para tirar o seu máximo de proveito, ela é geralmente usada em conjunto com o HTML. ♣")
+}
+function Css(){
+    alert("♣ Em JavaScript, você pode manipular CSS de três formas principais: Alterar estilos diretamente: Modificar propriedades CSS específicas de um elemento via JavaScript. Adicionar ou remover classes: Gerenciar classes CSS do elemento, aplicando ou removendo estilos predefinidos. Obter estilos computados: Acessar os estilos atuais aplicados ao elemento, incluindo os definidos em arquivos CSS. Essas técnicas permitem controlar dinamicamente a aparência dos elementos em uma página web. ♣")
+}
+function saida() {
+    alert("Obrigado por visitar nosso site! ");
+}
